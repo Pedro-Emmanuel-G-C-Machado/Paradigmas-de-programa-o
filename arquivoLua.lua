ad57aa349151ec7df429691f575ebb8c40362803
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2025-09-20 13:03:14 -0300

    Atividade 03- em lua

---
+++ arquivoLua.lua
@@ -0,0 +1,12 @@
+print("Hello world")
+
+local x = 2
+local y = 4
+local soma = x+y
+print(soma)
+
+function areaDoretangulo(base, altura)
+    return base*altura 
+end
+
+print("A area do triangulo:", areaDoretangulo(x, y))
